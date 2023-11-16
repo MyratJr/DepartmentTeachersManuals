aab691afeafa1a7922602f8d1af9c69e9764eb17
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2023-11-16 10:33:48 +0500

    hello

--- Project/admin.py
+++ Project/admin.py
@@ -3,20 +3,20 @@ from django.core.exceptions import ValidationError
 from django.contrib import admin
 from .models import *
 
-# class PropertyImageInline(admin.StackedInline):
-#     model = Daily
+class PropertyImageInline(admin.StackedInline):
+    model = Daily
 
-# class PropertyAdmin(admin.ModelAdmin):
-#     inlines = [ PropertyImageInline ]
-#     def save_model(self, request, obj, form, change):
-#         super().save_model(request, obj, form, change)
-#         if Daily.objects.count() >= 6:
-#             messages.error(request,'Cannot add more than 6 Daily objects')
-#     class Meta:
-#         model=Teacher
-# admin.site.register(Teacher,PropertyAdmin)
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [ PropertyImageInline ]
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if Daily.objects.count() >= 6:
+            messages.error(request,'Cannot add more than 6 Daily objects')
+    class Meta:
+        model=UserProfile
 
-admin.site.register(UserProfile)
-admin.site.register(Daily)
+admin.site.register(UserProfile,PropertyAdmin)
 admin.site.register(Lecture)
 admin.site.register(Degree)
+admin.site.register(Auditorium)
+admin.site.register(Group)
--- Project/models.py
+++ Project/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import User
     
@@ -5,10 +6,27 @@ class Lecture(models.Model):
     name=models.CharField(max_length=20)
     icon=models.ImageField()
 
+    def __str__(self):
+        return str(self.name)
+
+class Group(models.Model):
+    name=models.IntegerField()
+
+    def __str__(self):
+        return str(self.name)
+    
 class Degree(models.Model):
     name=models.CharField(max_length=20)
     icon=models.ImageField()
 
+    def __str__(self):
+        return str(self.name)
+
+class Auditorium(models.Model):
+    name=models.IntegerField(validators=[MaxValueValidator(9999)])
+
+    def __str__(self):
+        return str(self.name)
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -18,8 +36,11 @@ class UserProfile(models.Model):
     video=models.FileField(upload_to="videos")
     manuals=models.FileField(upload_to="manuals")
 
+    def __str__(self):
+        return str(self.user)
+
 class Daily(models.Model):
     property=models.ForeignKey(UserProfile,on_delete=models.CASCADE)
-    group=models.IntegerField()
-    lecture=models.CharField(max_length=30)
-    auditorium=models.IntegerField()
+    group=models.ForeignKey(Group,on_delete=models.CASCADE,related_name="Groups_for_teachers_lecture")
+    lecture=models.ForeignKey(Lecture,on_delete=models.CASCADE,related_name="Lectures_for_teachers_lecture")
+    auditorium=models.ForeignKey(Auditorium,on_delete=models.CASCADE,related_name="Auditoriums_for_teachers_lecture")
